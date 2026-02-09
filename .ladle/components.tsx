3877addbe7c207473e6554e5099959926ab7b93c
nokternol <nokternol@users.noreply.github.com>
2026-02-09 22:35:34 +0000

    configure ladle provider

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,6 +1,23 @@
 import type { GlobalProvider } from '@ladle/react';
 import '../src/styles/globals.css';
 
-export const Provider: GlobalProvider = ({ children }) => {
-  return <div className="min-h-screen bg-gray-900">{children}</div>;
+export const Provider: GlobalProvider = ({ children, globalState }) => {
+  const isDark = globalState.theme === 'dark';
+
+  return (
+    <div className={isDark ? 'dark' : ''}>
+      <div 
+        className={`
+          min-h-screen w-full p-12
+          ${isDark 
+            ? 'bg-surface-dark text-white' 
+            : 'bg-surface text-slate-950'}
+        `}
+      >
+        <div className="max-w-4xl mx-auto">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 };
