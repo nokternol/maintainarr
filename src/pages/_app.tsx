c7bea56f48962ef41cd2518cfef7d1107cb5cedc
nokternol <nokternol@users.noreply.github.com>
2026-02-11 20:30:53 +0000

    Apply default AppWrapper boundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { ErrorBoundary, ErrorFallback } from '@app/components/ErrorBoundary';
 import type { AppProps } from 'next/app';
 import '@app/styles/globals.css';
 
 function MaintainarrApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ErrorBoundary fallback={(error, reset) => <ErrorFallback error={error} reset={reset} />}>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
 
 export default MaintainarrApp;
